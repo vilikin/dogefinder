618e117e8caaed8e6eef2f351e8a6f48d0383df2
Vili Kinnunen <vili.kinnunen1@gmail.com>
2019-04-24 17:15:48 +0300

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,55 @@
+import env from 'dotenv';
+
+env.config();
+
+export interface Config {
+  sites: Site[];
+  mailgun: Mailgun,
+  receiver: string,
+}
+
+export interface Mailgun {
+  key: string,
+  domain: string
+}
+
+export interface Site {
+  name: string;
+  url: string;
+}
+
+export const config: Config = {
+  sites: [
+    {
+      name: "svk-toy",
+      url: "https://www.villakoirakerho.com/pentu_toy.html"
+    },
+    {
+      name: "svk-miniature",
+      url: "https://www.villakoirakerho.com/pentulistat_kaavil.html"
+    },
+    {
+      name: "maltankos",
+      url: "http://www.maltankos.com/Pentuja.html"
+    },
+    {
+      name: "poodlerocks",
+      url: "https://www.poodlerocks.com/puppies3"
+    },
+    {
+      name: "poutapaivan-blogspot",
+      url: "https://poutapaivan.blogspot.com/p/pentuja.html"
+    },
+    {
+      name: "lumijoen-kennel",
+      url: "https://lumijoenkennel.webnode.fi/maltasepuppy/"
+    }
+  ],
+  mailgun: {
+    key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
+  },
+  receiver: process.env.EMAIL
+};
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import config, { Site } from './config';
+import axios from 'axios';
+import BPromise from 'bluebird';
+import { readFileSync, writeFileSync } from 'fs';
+import { html } from "common-tags";
+import cron from 'node-cron';
+
+import mailgunJs from 'mailgun-js';
+
+const mailgun = mailgunJs({ apiKey: config.mailgun.key, domain: config.mailgun.domain });
+
+async function run(): Promise<void> {
+  await BPromise.each(config.sites, checkSiteForChanges);
+}
+
+async function checkSiteForChanges(site: Site) {
+  const { data } = await axios.get(site.url);
+  if (!data) return;
+
+  const cachedSite = getSiteFromCache(site);
+
+  if (cachedSite && hasSiteChanged(data, cachedSite)) {
+    await notifyErikaAboutSiteChange(site);
+  }
+
+  updateCache(site, data);
+}
+
+function getSiteFromCache(site: Site): string | null {
+  try {
+    return readFileSync(getCacheFilePathOfSite(site))
+      .toString();
+  } catch (e) {
+    return null;
+  }
+}
+
+function hasSiteChanged(currentHtml: string, cachedHtml: string): boolean {
+  return currentHtml !== cachedHtml;
+}
+
+function updateCache(site: Site, currentHtml: string) {
+  writeFileSync(getCacheFilePathOfSite(site), currentHtml);
+}
+
+function getCacheFilePathOfSite(site: Site) {
+  return `cache/${site.name}.html`;
+}
+
+async function notifyErikaAboutSiteChange(site: Site): Promise<void> {
+  console.log("site changed!!!!", site);
+
+  const data = {
+    from: `Dogefinder <dogefinder@${config.mailgun.domain}>`,
+    to: config.receiver,
+    subject: "Puppy news! \\(^O^)/",
+    html: html`
+        <p>Check out <b>${site.name}</b> has new puppy news for you! Awesome:D</p>
+        <p>Check the site:<br/>${site.url}</p> 
+    `
+  };
+
+  try {
+    const result = await mailgun.messages().send(data);
+    console.log(`Mail sent successfully!`);
+  } catch (e) {
+    console.log(`Error occurred while sending mail: ${e}`);
+  }
+}
+
+const hourly = '0 * * * *';
+cron.schedule(hourly, () => {
+  console.log(`Running DogeFinder at: ${new Date()}`);
+  run();
+});
